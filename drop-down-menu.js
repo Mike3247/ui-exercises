a2cc4a2f7015210d68701cde8e54b5c48a3f2e93
Mike Selmani <mike_selmani@hotmail.com>
2023-04-05 14:33:39 -0400

    pass toggling function for Home div display from none to block in click eventlistener run on menuDiv

--- drop-down-menu.js
+++ drop-down-menu.js
@@ -1,9 +1,19 @@
-const menuNav = document.getElementById("menu");
-const homeNav = document.getElementById("home");
-const aboutNav = document.getElementById("about");
-const contactNav = document.getElementById("contact");
-menuNav.addEventListener("click", () => {
-    homeNav.style.visibility = "visible";
-    contactNav.style.visibility = "visible";
-    aboutNav.style.visibility = "visible";
+const menuDiv = document.getElementById("menu");
+// const homeNav = document.getElementById("home");
+// const aboutNav = document.getElementById("about");
+// const contactNav = document.getElementById("contact");
+// const dropdownNavs = document.getElementsByClassName("dropdown");
+// const arrayOfDropdownNavs = Array.from(dropdownNavs);
+
+function toggleDisplayHome() {
+    const homeDiv = document.getElementById("home");
+    if (homeDiv.style.display === "none") {
+        homeDiv.style.display = "block";
+    } else {
+        homeDiv.style.display = "none";
+    }
+};
+
+menuDiv.addEventListener("click", () => {
+    toggleDisplayHome();
 });
