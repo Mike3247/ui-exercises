40c64e18f175af5d25383fb3e137dd27d926ae51
Mike Selmani <mike_selmani@hotmail.com>
2023-04-05 22:22:14 -0400

    add example-image-slider for learning purposes, add own images, left out some features of example

--- image-slider.js
+++ image-slider.js
@@ -1,4 +1,27 @@
-// const menuDiv = document.getElementById("menu");
+const rightArrow = document.getElementById("rightArrow");
+
+function moveRight () {
+    const images = document.getElementsByClassName("images");
+    const arrayOfImages = Array.from(images);
+    console.log(arrayOfImages);
+    arrayOfImages.forEach((item) => {
+        const countOccurrences = function(arr, val) {
+            return arr.reduce((acc, elem) => (val === elem ? acc + 1 : acc), 0);
+          };
+        
+        if (countOccurrences(arrayOfImages, arrayOfImages.indexOf(item)) === 0) {
+            item.style.display = "block";
+            arrayOfImages.forEach((item, 0))
+            }
+            else if (countOccurrences(arrayOfImages, arrayOfImages.indexOf(item)) === 1) {
+                    item.style.display = "none";
+                    }
+    })
+};
+
+rightArrow.addEventListener("click", () => {
+    moveRight();
+});
 
 // function toggleDisplayDivs() {
 //     const dropDivs = document.getElementsByClassName("dropDivs");
